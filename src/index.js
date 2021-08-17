e13a31d26204e651bbffa40d39b5ff9b86a71ef6
Megan McCarty <megan_mccarty@hotmail.com>
2021-08-17 09:19:32 -0400

    commit tree d571c5097c0ebec067c8c41d4dc0311641f5f697

--- src/index.js
+++ src/index.js
@@ -1,26 +1,40 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
-  // write your createStore code here
+export function createStore(reducer) {
+    // write your createStore code here
+    let state;
+
+    function dispatch(action) {
+        state = reducer(state, action);
+        render();
+    }
+
+    function getState() {
+        return state;
+    }
+
+    return { dispatch, getState };
 }
 
 export function candyReducer(state = [], action) {
-  switch (action.type) {
-    case "candies/add":
-      return [...state, action.candy];
-    default:
-      return state;
-  }
+    switch (action.type) {
+        case "candies/add":
+            return [...state, action.candy];
+        default:
+            return state;
+    }
 }
 
 function render() {
-  let container = document.getElementById("container");
-  if (store.getState()) {
-    container.textContent = store.getState().join(" ");
-  } else {
-    throw new Error("the store's state has not been defined yet");
-  }
+    let container = document.getElementById("container");
+    if (store.getState()) {
+        container.textContent = store.getState().join(" ");
+    } else {
+        throw new Error("the store's state has not been defined yet");
+    }
 }
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+let store = createStore(candyReducer);
+store.dispatch({ type: "@@INIT" });
